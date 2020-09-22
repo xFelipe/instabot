97b2ad819a55e040c07b708aba5549159cb44647
Felipe <xfelipegd@gmail.com>
2020-09-22 00:34:52 -0300

    Funcionalidade de 'seguir' implementada

--- bot/insta_bot.py
+++ bot/insta_bot.py
@@ -18,6 +18,9 @@ class InstaBot:
     OPEN_PHOTO_XPATH = r"/html/body/div[4]/div[2]/div/article/div[2]"
     WHO_LIKES_PHOTO = r"/html/body/div[4]/div[2]/div/article/div[3]/section[2]/div/div[2]/button"  # NOQA
     WHO_LIKES_DIV = "/html/body/div[5]/div[@role='dialog']"
+    FOLLOW_WHO_LIKE = r"/html/body/div[5]/div/div/div[2]/div/div/div[{user_position}]/div[3]/button[text()='Seguir']"  # NOQA
+    UNFOLLOW_WHO_LIKE = r"/html/body/div[5]/div/div/div[2]/div/div/div[{user_position}]/div[3]/button[text()='Seguindo']"  # NOQA
+    USER_UNFOLLOW_CONFIRM = r"/html/body/div[6]/div/div/div/div[3]/button[1][text()='Deixar de seguir']"  # NOQA
 
     # r"/html/body/div[4]/div/div/div/div[3]/button[2]" | type="text"
 
@@ -46,15 +49,15 @@ class InstaBot:
     def login(self, username, password):
         """Faz login no no instagram e tira mensagens iniciais"""
         self.driver.get(self.INSTAGRAM_URL)
-        find_element_by_xpath = self.driver.find_element_by_xpath
+        find_element = self.driver.find_element_by_xpath
 
-        find_element_by_xpath(self.LOGIN).send_keys(username)
-        find_element_by_xpath(self.PASSWORD).send_keys(password)
-        find_element_by_xpath(self.LOGIN_SUBMIT).click()
+        find_element(self.LOGIN).send_keys(username)
+        find_element(self.PASSWORD).send_keys(password)
+        find_element(self.LOGIN_SUBMIT).click()
         try:
-            find_element_by_xpath(self.NOT_SAVE_LOGIN_DATA).click()
+            find_element(self.NOT_SAVE_LOGIN_DATA).click()
             sleep(1)
-            find_element_by_xpath(self.NOT_SAVE_LOGIN_DATA).click()
+            find_element(self.NOT_SAVE_LOGIN_DATA).click()
             sleep(1)
         except NoSuchElementException:
             pass
@@ -74,6 +77,7 @@ class InstaBot:
             self.USER_PHOTO.format(line=line, column=column)
         )
         photo.click()
+        sleep(1)
 
     def a_photo_is_open(self):
         return self._have_element_by_xpath(self.OPEN_PHOTO_XPATH)
@@ -84,3 +88,35 @@ class InstaBot:
 
     def likes_list_is_open(self):
         return self._have_element_by_xpath(self.WHO_LIKES_DIV)
+
+    def is_following_user(self, user_position):
+        user_is_not_followed = self.FOLLOW_WHO_LIKE.format(user_position=user_position)
+        user_is_followed = self.UNFOLLOW_WHO_LIKE.format(user_position=user_position)
+        if self._have_element_by_xpath(user_is_not_followed):
+            return False
+        elif self._have_element_by_xpath(user_is_followed):
+            return True
+        else:
+            return NoSuchElementException(
+                "Impossível determinar se usuário foi foi seguido."
+            )
+
+    def follow_user(self, user_position):
+        find_element = self.driver.find_element_by_xpath
+        if not self.likes_list_is_open():
+            raise NoSuchElementException(
+                "A tela de usuários que curtiram fotos não está aberta"
+            )
+        follow_user = find_element(
+            self.FOLLOW_WHO_LIKE.format(user_position=user_position)
+        )
+        follow_user.click()
+
+    def unfollow_user(self, user_position):
+        find_element = self.driver.find_element_by_xpath
+        if not self.likes_list_is_open():
+            raise NoSuchElementException(
+                "A tela de usuários que curtiram fotos não está aberta"
+            )
+        find_element(self.UNFOLLOW_WHO_LIKE.format(user_position)).click()
+        find_element(self.USER_UNFOLLOW_CONFIRM).click()
--- tests/testcases/navigation_test.py
+++ tests/testcases/navigation_test.py
@@ -23,6 +23,18 @@ def test_open_users_who_liked_photo(authenticated_bot):
     bot.go_to_home_of(ANOTHER_USER)
     bot.open_user_photo(1, 1)
     bot.open_who_liked_photo()
-    from time import sleep; sleep(5)
     assert bot.likes_list_is_open(),\
         "A tela de listas de pessoas que curtiram não abriu."
+
+def test_follow_first_user_who_liked_photo(authenticated_bot):
+    first_user = 1
+    bot = authenticated_bot
+    bot.go_to_home_of(ANOTHER_USER)
+    bot.open_user_photo(1, 1)
+    bot.open_who_liked_photo()
+    bot.follow_user(first_user)
+    is_following_user = bot.is_following_user(first_user)
+    bot.unfollow_user(first_user)
+    from time import sleep; sleep(10)  # Em teste
+    assert is_following_user,\
+        "Erro ao seguir usuário que curtiu a foto selecionada."
